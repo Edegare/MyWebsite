9047fa9e37b09f23de9a86f939179862191e0334
Edgar Ferreira <edgarcferreira03@gmail.com>
2025-06-17 15:19:47 +0100

    feat: More info Content start

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from '@components/Header'
+import Content from '@components/Content'
 import Menu from '@/components/Menu'
 import Avatar from './components/Avatar'
 
@@ -30,6 +31,10 @@ function App() {
               <div>
                 <Avatar isDarkMode={isDarkMode} currentSection={currentSection}/>
               </div>
+            </div>
+            {/* Bottom Section - More info */}
+            <div>
+              <Content isDarkMode={isDarkMode} currentSection={currentSection} />
             </div>
           </div>
         </div>
---
+++ my-portfolio/src/components/Content.tsx
@@ -0,0 +1,63 @@
+
+const projects = [
+    {
+        name: "Pascal Compiler",
+        tech: ["Python","PLY", "EWVM"],
+        rarity: "Mythical",
+        link: "https://github.com/Edegare/Pascal-Compiler-PL",
+        description: "A modular Python compiler that translates standard Pascal code into EWVM machine code, featuring full lexical, syntactic, semantic analysis and an interactive AST visualizer.",
+    },
+    {
+        name: "3D Engine",
+        tech: ["C++", "OpenGL"],
+        rarity: "Legendary",
+        link: "https://github.com/Edegare/3DEngine-CG",
+        description: "A mini 3D engine based on a scene graph structure, that includes a geometry generator and supports hierarchical transformations, animations, VBOs, textures and lightning.",
+    },
+    {
+        name: "Secure Vault",
+        tech: ["Python"],
+        rarity: "Epic",
+        link: "https://github.com/Edegare/Secure-Vault-SSI",
+        description: "A secure client-server vault written in python for encrypted file storage and sharing, featuring X.509 authentication, AES-GCM encryption, and CLI-based access control",
+    },
+    {
+        name: "Distributed Network Monitoring System",
+        tech: ["Go","CORE"],
+        rarity: "Rare",
+        link: "https://github.com/Edegare/Network-Monitoring-System-CC",
+        description: "Distributed Network Monitoring system with custom UDP and TCP protocols for collecting and alerting network metrics",
+    },
+]
+
+const about = "";
+
+const skills = [
+    {name: "Java", level: 75, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" },
+    {name: "C", level: 70, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" },
+    {name: "C++", level: 70, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" },
+    {name: "C#", level: 70, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" },
+    {name: "Python", level: 65, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" },
+    {name: "SQL", level: 70, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" },
+    {name: "GO", level: 75, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original-wordmark.svg" },
+    {name: "JavaScript", level: 85, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" },
+    {name: "React", level: 90, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" },
+    {name: "CSS", level: 70, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" },
+    {name: "HTML", level: 75, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" },
+    {name: "Git", level: 80, icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" },
+]
+
+const contact = {
+    text: "Ready to start our next adventure together?",
+    email: "mailto:edgarcferreira03@gmail.com",
+    github: "https://github.com/Edegare",
+    linkedin: "https://www.linkedin.com/in/edgar-ferreira-1aa033329/",
+}
+
+export default function Content({ isDarkMode, currentSection }: { isDarkMode: boolean; currentSection: string }) {
+
+    return (
+        <>
+        </>
+    )
+}
