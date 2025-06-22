fb778c36578868ed8541dcdeb9e0ab85ceed5fe9
Edgar Ferreira <edgarcferreira03@gmail.com>
2025-06-22 19:52:12 +0100

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from '@components/Header'
 import Content from '@components/Content'
 import Menu from '@/components/Menu'
-import Avatar from './components/Avatar'
+import Avatar from '@/components/Avatar'
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true)
